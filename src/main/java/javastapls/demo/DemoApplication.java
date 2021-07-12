008abd53b5cc311373d889f4c38a19a8ddd51606
anquisbarr <sebastian.quispe.b@utec.edu.pe>
2021-07-12 03:55:56 -0500

    CROS?

--- src/main/java/javastapls/demo/DemoApplication.java
+++ src/main/java/javastapls/demo/DemoApplication.java
@@ -3,8 +3,11 @@ package javastapls.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan({"javastapls.controller","javastapls.business"})
@@ -14,4 +17,15 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/course/**").allowedOrigins("http://localhost:3000/").
+                allowedMethods("GET");
+			}
+		};
+	}
 }
