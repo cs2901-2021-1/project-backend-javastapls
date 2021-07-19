049184545a78a4ea9ecd46e4f8e57bbc8a2d1d4c
anquisbarr <sebastian.quispe.b@utec.edu.pe>
2021-07-19 03:46:06 -0500

    OAuth fully functional

--- src/main/java/javastapls/demo/DemoApplication.java
+++ src/main/java/javastapls/demo/DemoApplication.java
@@ -1,16 +1,19 @@
 package javastapls.demo;
 
+import javastapls.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
-@SpringBootApplication
-@ComponentScan({"javastapls.controller","javastapls.business"})
-@EntityScan("javastapls.data")
+@ComponentScan({"javastapls.controller", "javastapls.business", "javastapls.config", "javastapls.exception", "javastapls.payload", "javastapls.security" , "javastapls.util"})
+@EntityScan({"javastapls.data","javastapls.model"})
 @EnableJpaRepositories("javastapls.data.repositories")
+@EnableConfigurationProperties(AppProperties.class)
+@SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
