41d0be5c1c27f77136ffc50afa73e2266bcdb75c
JohanTv <johan.tanta@utec.edu.pe>
2021-07-19 05:30:10 -0500

    1 code smells

--- src/main/java/javastapls/exception/ResourceNotFoundException.java
+++ src/main/java/javastapls/exception/ResourceNotFoundException.java
@@ -1,12 +1,10 @@
 package javastapls.exception;
 
-import java.io.Serializable;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException implements Serializable {
+public class ResourceNotFoundException extends RuntimeException {
     private final String resourceName;
     private final String fieldName;
     private final Object fieldValue;
